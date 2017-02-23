53307c6d76a492d80a5a8707d2a015fb8a8192f7
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-02-23 18:07:04 +0100

    wip

--- src/ecg.erl
+++ src/ecg.erl
@@ -4,10 +4,39 @@
 %% API exports
 -export([main/1]).
 
-main([ModS]) ->
+%% Top-level commands
+-export([full_graph/1,
+         component/2]).
+
+main(["full", ModS])                 -> full_graph(ModS);
+main(["component", Component, ModS]) -> component(Component, ModS).
+
+full_graph(ModS) ->
     {ok, Calls} = get_calls(ModS),
     %print("calls: ~p\n", [Calls]),
     ok = make_dot(ModS, Calls).
+
+component(ComponentSpec, ModS) ->
+    {ok, Calls} = get_calls(ModS),
+    G = digraph:new(),
+    [ begin digraph:add_vertex(G, From),
+            digraph:add_vertex(G, To),
+            digraph:add_edge(G, From, To) end
+      || {From, To} <- Calls ],
+    Components = digraph_utils:components(G),
+    print("components: ~p\n", [Components]),
+    SubG = digraph_utils:subgraph(G, select_component(ComponentSpec, Components)),
+    print("edges: ~p\n", [digraph:edges(SubG)]).
+
+select_component(ComponentSpec, Components) ->
+    CompMFA = parse_component_spec(ComponentSpec),
+    %% Assert the list is one element long. It should be.
+    [Needle] = [ Comp || Comp <- Components, lists:member(CompMFA, Comp) ],
+    Needle.
+
+parse_component_spec(ComponentSpec) ->
+    [MS, FS, AS] = string:tokens(ComponentSpec, ":/"),
+    {list_to_atom(MS), list_to_atom(FS), list_to_integer(AS)}.
 
 get_calls(ModS) ->
     Mod = list_to_atom(ModS),
