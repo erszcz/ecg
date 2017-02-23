5fab57cca29d1d5d8e24f0241e9b9b0491716cb7
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-02-23 14:31:29 +0100

    Generate a GraphViz .dot file describing a module call graph

--- src/ecg.erl
+++ src/ecg.erl
@@ -1,13 +1,38 @@
 -module(ecg).
+-mode(compile).
 
 %% API exports
--export([]).
+-export([main/1]).
 
-%%====================================================================
-%% API functions
-%%====================================================================
+main([ModS]) ->
+    {ok, Calls} = get_calls(ModS),
+    %print("calls: ~p\n", [Calls]),
+    ok = make_dot(ModS, Calls).
 
+get_calls(ModS) ->
+    Mod = list_to_atom(ModS),
+    ModFile = code:which(Mod),
+    {ok, _} = xref:start(xr()),
+    {ok, _} = xref:add_module(xr(), ModFile),
+    {ok, _} = xref:q(xr(), lists:flatten(["E | ", ModS]),
+                     [{verbose, false}]).
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+make_dot(Name, Edges) ->
+    Dot = ["digraph ", Name, " {\n",
+           make_dot_(Edges),
+           "}\n"],
+    %print("dot: ~p\n", [Dot]),
+    %print("dot file: ~p\n", [DotFile]),
+    print("~ts\n", [Dot]).
+
+make_dot_(Edges) ->
+    [ ["  ", format_mfa(From), " -> ", format_mfa(To), ";\n"]
+      || {From, To} <- Edges ].
+
+format_mfa({M, F, A}) -> io_lib:format("\"~ts:~ts/~b\"", [M, F, A]).
+
+print(Fmt, Args) ->
+    io:format(Fmt, Args).
+
+%% xref handle
+xr() -> ?MODULE.
